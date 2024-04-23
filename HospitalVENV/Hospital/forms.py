b7fc0958a3a7cce7864f958dd53c7213b7c40df7
ThanhBinh101 <118110269+ThanhBinh101@users.noreply.github.com>
2024-04-23 15:34:15 +0700

    he

--- HospitalVENV/Hospital/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class UserForm(forms.Form):
-    gmail = forms.CharField(max_length=100)
-    name = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
-    date = forms.CharField(max_length=100)
-    gender = forms.CharField(max_length=100)
